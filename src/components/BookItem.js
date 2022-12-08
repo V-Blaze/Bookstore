bff21fb602e29a7589f03376792c8098e651d3d0
V-Blaze <valentine6586@gmail.com>
2022-12-08 10:26:49 +0100

    Create core project componets and add routiing with react router

---
+++ src/components/BookItem.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const BookItem = () => (
+  <div>BookItem</div>
+);
+
+export default BookItem;
---
+++ src/components/InputBook.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const InputBook = () => (
+  <div>
+    <form>
+      <input type="text" placeholder="Book titile ..." />
+      <input type="text" placeholder="Book author ..." />
+      <button type="submit">Add Book</button>
+    </form>
+  </div>
+);
+
+export default InputBook;
