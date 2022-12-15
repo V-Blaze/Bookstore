41ac2d327913d6e3ad9902e96ee3af1712513dc8
V-Blaze <valentine6586@gmail.com>
2022-12-15 12:27:21 +0100

    Add HTML markup to BookItemJS to meet the Zeplin design

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -3,26 +3,52 @@ import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { removeBook } from '../redux/books/books';
 
+// Stylesheet
+import './BookItem.css';
+
 const BookItem = ({ book }) => {
   const dispatch = useDispatch();
   // eslint-disable-next-line
   const { title, author, item_id } = book;
   return (
     <>
-      <div>
-        <h4>{title}</h4>
-        <h6>{author}</h6>
+      <div className="book-item-main-div">
+        <div className="title-section">
+          <div>
+            <span className="book-category">Fiction</span>
+            <h2 className="book-title">{title}</h2>
+            <h6 className="book-author">{author}</h6>
+          </div>
+          <div className="book-action-btns">
+            <button type="button">Commnents</button>
+            <button
+              type="button"
+              onClick={(e) => {
+                e.preventDefault();
+                // eslint-disable-next-line
+                dispatch(removeBook(item_id)).unwrap();
+              }}
+            >
+              Remove
+            </button>
+            <button type="button">Edit</button>
+          </div>
+        </div>
+        <div className="progress-container">
+          <div className="progress-section">
+            <div className="circular-progress" />
+            <div>
+              <h3 className="circular-progress-percent">65%</h3>
+              <span>Completed</span>
+            </div>
+          </div>
+          <div className="current-chapter-section">
+            <h3 className="cur-chapter">CURRENT CHAPTER</h3>
+            <h4>Chapter 10</h4>
+            <button type="button">UPDATE PROGRESS</button>
+          </div>
+        </div>
       </div>
-      <button
-        type="button"
-        onClick={(e) => {
-          e.preventDefault();
-          // eslint-disable-next-line
-          dispatch(removeBook(item_id)).unwrap();
-        }}
-      >
-        Remove
-      </button>
     </>
   );
 };
